65506266dee1f6881855bc818de4d999357cf009
colo76 <martinbaez@gmail.com>
2015-04-26 19:27:38 -0300

    Cmabio de la definicion de los MBeans

--- src/main/java/ar/gov/mecon/dgsiaf/bi/mbeans/RelinquerMBean.java
+++ src/main/java/ar/gov/mecon/dgsiaf/bi/mbeans/RelinquerMBean.java
@@ -1,31 +1,29 @@
 package ar.gov.mecon.dgsiaf.bi.mbeans;
 
+import org.apache.log4j.Logger;
 import org.springframework.jmx.export.annotation.ManagedAttribute;
 import org.springframework.jmx.export.annotation.ManagedOperation;
 import org.springframework.jmx.export.annotation.ManagedResource;
 
-@ManagedResource(objectName="bean:name=relinquerbean", description="Relinquer Bean", 
-logFile="jmx.log", currencyTimeLimit=15, persistPolicy="OnUpdate", persistPeriod=200,
-persistLocation="foo", persistName="bar")
+@ManagedResource(objectName = "com.colo:name=relinquerbean,Type=weblogic.management.mbeanservers.domainruntime.DomainRuntimeServiceMBean)", description = "Relinquer Bean", logFile = "relinquer.log")
 public class RelinquerMBean implements IRelinquerMbean {
 
-    private boolean enabled;
+	private boolean enabled;
 
+	private Logger LOGGER = Logger.getLogger(RelinquerMBean.class);
 
 	public void setEnabled(boolean enabled) {
 		this.enabled = enabled;
 	}
 
+	@ManagedOperation(description = "Relinq")
+	public void testRelinquerMbean() {
+		LOGGER.info("Invocando testRelinquerMbean");
 
-	@ManagedOperation(description="Relinq")
-	public void testRelinquerMbean(){
-		
 	}
-    
-    
-    @ManagedAttribute(description = "Audit enabled")
-    public boolean getEnabled()
-    {
-       return this.enabled;
-    }
+
+	@ManagedAttribute(description = "enabled")
+	public boolean getEnabled() {
+		return this.enabled;
+	}
 }
